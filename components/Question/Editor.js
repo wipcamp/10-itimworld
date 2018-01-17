434847b41b89684127e4e40773749831791bcb06
SandzoNight <flukemdc@gmail.com>
2018-01-18 00:49:48 +0700

    [UPDATE] found bug on setState of ReactQuill

--- components/Question/Editor.js
+++ components/Question/Editor.js
@@ -1,37 +1,51 @@
-import React from 'react'
-import { Field } from 'redux-form'
+import React from 'react';
+import { Field } from 'redux-form';
+// import ReactQuill from 'react-quill';
 
 export default class Editor extends React.Component {
-  constructor(props) {
-    super(props)
+  state = { 
+    val: ''
+   }
+
+  componentWillMount() {
     if (typeof window !== 'undefined') {
       this.ReactQuill = require('react-quill')
+      this.reactQuillRef = null
     }
+  }
+
+  handle(e, input) {
+    setTimeout(() => {
+      input.onChange(e)
+    }, 0)
+  }
+
+  forceFocus() {
+    this.reactQuillRef.focus()
   }
 
   render() {
-    const ReactQuill = this.ReactQuill
-    const renderQuill = ({ input }) => {
-      if (typeof window !== 'undefined' && ReactQuill) {
-        return (
-          <ReactQuill
-            {...input}
-            onChange={(newValue, delta, source) => {
-              if (source === 'user') {
-                input.onChange(newValue);
-              }
-            }}
-            onBlur={(range, source, quill) => {
-              input.onBlur(quill.getHTML());
-            }}
-            theme={"snow"}
-          />
-        )
-      } else {
-        return <textarea />;
-      }
-    }
-    return <Field name="description" component={renderQuill} />
-  }
     
+    const ReactQuill = this.ReactQuill
+    const renderQuill = ({name, input, value}) => {
+      if (typeof window !== 'undefined' && ReactQuill)
+        return (
+          <div>
+            <ReactQuill
+              {...input}
+              ref={(el) => { this.reactQuillRef = el }}
+              onChange={e => this.handle(e, input)
+              }
+              onBlur={() => this.forceFocus()}
+              theme="snow"
+              value={input.value}
+            />
+          </div>
+        )
+      return <div />
+    }
+    return (
+      <Field name="answer" component={renderQuill} />
+    )
+  }
 }
