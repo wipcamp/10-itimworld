c57df28cf5eeef7c9f8d6200267d12613a1b9d41
basinw <bas10600@gmail.com>
2018-01-24 20:44:20 +0700

    [UPDATE] Update register UI change font to Pridi, and styling radio button

--- components/Register/Main.js
+++ components/Register/Main.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { compose } from 'recompose'
 import { Field, reduxForm } from 'redux-form'
-import styled from 'styled-components'
+import { connect } from 'react-redux'
+import styled, { injectGlobal } from 'styled-components'
 import {
   header,
   information_fields,
@@ -23,6 +24,40 @@ import {
 import {actions as registerActions} from '../../store/reducers/register'
 import dataDropdown from './data-dropdown.json'
 
+injectGlobal`
+
+  body {
+    font-family: 'pridi-light', serif;
+    font-size: 15px;
+  }
+
+  * {
+    ::-webkit-scrollbar {
+      width: 10px;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+      /* background: #f1f1f1; */
+      border-radius: 20px;
+      margin: 10px;
+      /* background: pink; */
+
+    }
+  
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+      border-radius: 20px;
+      background: #768694;
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+      background: #768694;
+    } 
+  }
+`
+
 const renderField = ({
   label,
   input,
@@ -41,9 +76,10 @@ const templateInput = ({
   input,
   type,
   meta: { touched, error, warning },
-  className
+  className,
+  placeholder
 }) => (
-  <Input {...input} type={type} className={className} />
+  <Input {...input} type={type} placeholder={placeholder} className={className} />
   // {touched && error && <span className='error'>{error}</span>}
 )
 
@@ -55,11 +91,10 @@ const templateSelect = ({
   middleClass,
   dropdown,
   width = '100%',
-  values,
-  separator
+  values
 }) => (
   <div className={middleClass}>
-    <Select {...input} type={type} className={className} width={width} separator={separator}>
+    <Select {...input} type={type} className={className} width={width}>
       <option value='' >โปรดเลือก</option>
       {
         dropdown.map((v, i) => (
@@ -104,7 +139,7 @@ const templateRadio = ({
   className
 }) => {
   return (
-    <Input {...input} type={type} className={className} />
+    <Radio {...input} type={type} className={className} />
   )
 }
 
@@ -135,9 +170,10 @@ const BackgroundContainer = styled.div`
 `
 
 const inputStyle = {
-  backgroundColor: '#D6D6D6',
-  color: '#fff',
-  minHeight: '38px'
+  backgroundColor: '#f5f8fa',
+  color: '#768694',
+  minHeight: '38px',
+  borderColor: '#ced4da'
 }
 
 const Input = styled.input`
@@ -146,7 +182,6 @@ const Input = styled.input`
   border-radius: 10px;
   padding: 4px 15px;
   outline: 0;
-  border: 0;
   color: ${inputStyle.color};
   min-height: ${inputStyle.minHeight};
 
@@ -160,24 +195,90 @@ const TextArea = styled.textarea`
   color: ${inputStyle.color};
 `
 
+const RadioContainer = styled.div`
+  position: relative;
+
+  & > label {
+    display: inline-block;
+    position: relative;
+    left: -12px;
+    padding: 2px 0px 2px 35px;
+    margin: 0px auto;
+    height: 30px;
+    z-index: 9;
+    cursor: pointer;
+    transition: all 0.25s linear;
+  }
+
+  & > input[type=radio] {
+    position: absolute;
+    visibility: hidden;
+
+    &:checked ~ div.check::before {
+      background: ${inputStyle.color};
+    }
+  }
+
+  & > div.check {
+    display: block;
+    position: absolute;
+    border: 1px solid ${inputStyle.borderColor};
+    border-radius: 100%;
+    height: 20px;
+    width: 20px;
+    top: 4px;
+    left: 5px;
+    z-index: 5;
+    transition: border .25s linear;
+    background: ${inputStyle.backgroundColor};
+
+    &::before {
+      display: block;
+      position: absolute;
+      content: '';
+      border-radius: 100%;
+      height: 12px;
+      width: 12px;
+      top: 3px;
+      left: 3px;
+      margin: auto;
+      transition: background 0.25s linear;
+    }
+  }
+`
+
+const Radio = styled.input`
+  &:checked ~ div.check::before {
+    background: ${inputStyle.color};
+  }
+`
+
 const RegisterSection = styled.form`
-  color: #0E3559;
+  color: #032e51;
   background: #fff;
   margin-bottom: 5em;
   border-radius: 10px;
-  /* box-shadow: 0px 5px 10px 2px rgba(0,0,0,0.5); */
 `
 
 const Label = styled.label`
   /* min-height: 50px; */
   padding-left: 10px;
+  font-size: 20px;
+`
+
+const Error = styled.small`
+  min-height: 18px;
+  color: #ea0c0c;
+  padding: 0 10px;
+  display: block;
+
 `
 
 const Select = styled.select`
   width: ${props => props.width};
-  background-color: #D6D6D6;
+  background-color: ${inputStyle.backgroundColor};
   border-radius: 10px;
-    color: white;
+  color: ${inputStyle.color};
 
   & select {
     appearance: none;
@@ -191,9 +292,6 @@ const Select = styled.select`
   &:last-child:after {
     content: "";
   }
-
-  ${props => props.separator && `
-  `}
 `
 
 const SubmitButton = styled.button`
@@ -207,14 +305,15 @@ const SubmitButton = styled.button`
 `
 
 export const MainRegister = props => {
-  const { handleSubmit, pristine, submitting } = props
+  const { handleSubmit, pristine, submitting, saveRegister } = props
+  console.log('node', process.env)
   return (
     <div>
       <BackgroundContainer>
         <div className='container'>
           <div className='row '>
-            <div className='col-12 text-center'>
-              <StyledHeader />
+            <div className='col-12 col-md-6 mx-auto text-center justify-content-center'>
+              <img src='https://wip.camp/assets/img/logo/wipcamp9-full-transparent.svg' className='img-fluid' alt='wipcamp-logo' />
             </div>
             <div className='col-12 text-center text-white'>
               <div>
@@ -222,12 +321,9 @@ export const MainRegister = props => {
               </div>
             </div>
             <div className='col-12 col-sm-10 mx-auto text-center'>
-              <RegisterSection onSubmit={handleSubmit(registerActions.saveRegister)}>
+              <RegisterSection onSubmit={handleSubmit(saveRegister)}>
                 <SubHeader>ลงทะเบียน</SubHeader>
-                <div className='row px-4 pb-5 pt-3'>
-                  <div className='col-12 text-dark'>
-                    <h2>ข้อมูลส่วนตัว</h2>
-                  </div>
+                <div className='row px-3 pb-5 pt-3'>
                   {
                     information_fields.map((e, i) => (
                       <div key={i} className={e.outerClass}>
@@ -237,7 +333,9 @@ export const MainRegister = props => {
                           component={templateInput}
                           type={e.type}
                           className={e.innerClass}
+                          placeholder={e.placeholder}
                         />
+                        <Error>ทดสอบ</Error>
                       </div>
                     ))
                   }
@@ -249,6 +347,7 @@ export const MainRegister = props => {
                         {
                           dob_fields.data.map((e, i) => (
                             <Field
+                              key={i}
                               name={e.name}
                               component={templateSelect}
                               className={e.innerClass + ' col'}
@@ -269,23 +368,27 @@ export const MainRegister = props => {
                       name={citizen_field.name}
                       component={templateInput}
                       className={citizen_field.innerClass}
+                      placeholder={citizen_field.placeholder}
                     />
                   </div>
                   <div className={`d-sm-inline-block d-none col-4`} />
                   <div className={gender_field.outerClass}>
+                    
                     <Label>{gender_field.label}</Label>
                     <div>
                       {
                         gender_field.data.map((e, i) => (
-                          <div key={i} className={e.innerClass}>
+                          <RadioContainer key={i} className={e.innerClass}>
                             <Field
-                              name={gender_field.name}
-                              component={'input'}
-                              value={`${i + 1}`}
                               type={'radio'}
+                              name={gender_field.name}
+                              component='input'
+                              value={`${i + 1}`}
+                              id={`${gender_field.name}-${e.label}-option`}
                             />
-                            <label className=' pl-0' htmlFor='gender-input' >{e.label}</label>
-                          </div>
+                            <Label htmlFor={`${gender_field.name}-${e.label}-option`}>{e.label}</Label>
+                            <div htmlFor={`${gender_field.name}-${e.label}-option`} className='check' />
+                          </RadioContainer>
                         ))
                       }
                     </div>
@@ -297,6 +400,7 @@ export const MainRegister = props => {
                       component={templateInput}
                       type={telno_field.type}
                       className={telno_field.innerClass}
+                      placeholder={telno_field.placeholder}
                     />
                   </div>
                   {
@@ -330,7 +434,7 @@ export const MainRegister = props => {
                       component={templateSelect}
                       className={religion_field.innerClass}
                       dropdown={religion_field.dropdown}
-                      values={religion_field.dropdown}
+                      values={religion_field.values}
                     />
                   </div>
 
@@ -362,6 +466,7 @@ export const MainRegister = props => {
                       component={templateInput}
                       type={gpax_field.type}
                       className={gpax_field.innerClass}
+                      placeholder={gpax_field.placeholder}
                     />
                   </div>
                   <div className={major_field.outerClass}>
@@ -382,6 +487,7 @@ export const MainRegister = props => {
                           name={e.name}
                           component={templateInput}
                           className={e.innerClass}
+                          placeholder={e.placeholder}
                         />
                       </div>
                     ))
@@ -394,6 +500,7 @@ export const MainRegister = props => {
                           name={e.name}
                           component={templateInput}
                           className={e.innerClass}
+                          placeholder={e.placeholder}
                         />
                       </div>
                     ))
@@ -437,6 +544,11 @@ export const MainRegister = props => {
 }
 
 export default compose(
+  connect(
+    state => ({
+      temp: state
+    }),
+    {...registerActions}),
   reduxForm({
     form: 'register',
     initialValues: {
