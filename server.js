93df68889199ee34fa07482923081723e7c0bc42
Keerati <mixxdarkz2@gmail.com>
2018-02-08 17:22:37 +0700

    [ADD] Add Server.js for Run Express and Nextjs

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+  .then(() => {
+    const server = express()
+
+    server.get('/robot.txt', (req, res) => {
+      return res.sendFile(res, './static/robot.txt')
+    })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
