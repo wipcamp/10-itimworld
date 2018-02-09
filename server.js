07fd9e8e590396bd694150376a82ea6800f118af
Keerati <mixxdarkz2@gmail.com>
2018-02-09 15:21:48 +0700

    [ADD] Add Route from next routes and redirect to landing

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ require('dotenv').config()
 
 const next = require('next')
 const path = require('path')
+const routes = require('./routes')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler()
+
+const handle = routes.getRequestHandler(app, ({req, res, route, query}) => {
+  app.render(req, res, '/', query)
+  // app.render(req, res, route.page, query)
+})
+
 
 app.prepare()
   .then(() => {
