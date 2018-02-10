b8e54993b34a5e321ddcb51eef0ab7b476f84460
Keerati <mixxdarkz2@gmail.com>
2018-02-10 12:49:44 +0700

    [ADD] Add High Order Check Cookie in Client

--- pages/register/index.js
+++ pages/register/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import RegisterPage from '../../components/Register/Main'
 import { compose } from 'recompose'
 
+import withRedux from '../../store/wrapper'
 import serverRender from '../../utils/serverRender'
 import clientRender from '../../utils/clientRender'
-import withRedux from '../../store/wrapper'
 
 const Register = props => (
   <RegisterPage {...props} />
@@ -13,5 +13,5 @@ const Register = props => (
 export default compose(
   withRedux(),
   clientRender(`/`),
-  serverRender()
+  serverRender(`/`)
 )(Register)
