9867ae459898355118cebb59984ff637c7fcc45f
Keerati <mixxdarkz2@gmail.com>
2018-02-10 12:44:45 +0700

    [UPDATE] Update Server Side HighOrder

--- utils/serverRender.js
+++ utils/serverRender.js
@@ -1,10 +1,11 @@
 import cookieParser, {checkToken} from '../utils/cookie'
 
 export default (path) => (Component) => {
-  Component.getInitialProps = async ({...ctx, req, res}) => {
-    if (ctx.req.headers.cookie) {
-      let token = await cookieParser(ctx).token
-      checkToken(res, token, path)
+  Component.getInitialProps = async (ctx) => {
+    let token = {}
+    if (ctx) {
+      token = await cookieParser(ctx).token
+      checkToken(ctx.res, token, path)
     }
   }
   return Component
