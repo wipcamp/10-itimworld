4051d5733afff2bfbd11ba814be953fb78661e09
SandzoNight <flukemdc@gmail.com>
2018-02-10 20:53:22 +0700

    [UPDATE] apply UI design to dashboard page

--- components/Dashboard/Main.js
+++ components/Dashboard/Main.js
@@ -10,18 +10,11 @@ import Header from '../Core/Header/Main'
 import Alert from '../Core/Alert'
 
 const BackgroundContainer = styled.div`
-  background: #B8D0EC;
+  background: #29241B url('/static/img/bg.png') center top;
   min-height: 100vh;
   height: auto;
-  background-image: url('/static/img/bg2-01.png');
   background-size: cover;
   background-attachment: fixed;
-
-
-  @media screen and (min-width: 576px) {
-    background-image: url('/static/img/bg-d2.png');
-    background-position-x: 50%;
-  }
 `
 
 const loadingIcon = keyframes`
@@ -48,6 +41,38 @@ const CardUpload = styled.div`
   color: #B8D0EC;
   user-select: none;
   position: relative;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center top;
+  
+  ${props => !props.filePath?`
+    background-image: url(${props.img});
+    `:`
+    background-image: url(${props.img.substring(0,`${props.img.length}`-4)}yes.png);
+    ;
+    `}
+  height: 290px;
+  width: 248px;
+  transition: all .5s;
+  margin: 0 auto;
+  border-radius: 10px;
+
+  @media (min-width: 768px) and (max-width: 991.98px) {
+    height: 232px;
+    width: 200px;
+  }
+
+  @media (max-width: 575.98px) {
+    ${props=>props.link?'':'margin-top: 20px;'}
+    height: 236px;
+    width: 201px;
+  }
+
+  &:hover {
+    cursor:pointer;
+    transform: scale(1.010);
+    box-shadow: 0 8px 30px rgba(0,0,0,0.5);
+  }
 
   & input[type=file] {
     position: absolute;
@@ -110,21 +135,16 @@ const CardUpload = styled.div`
   }
 
   & .dropzone {
-    margin: 20px auto;
+    margin: 0px auto;
     display: flex;
     justify-content: center;
-    width: 290px;
-    height: 230px;
+    height: 100%;
     cursor: pointer;
 
     background-size: cover;
-    background-image: url(${props => props.img});
+    ${`/*background-image: url(${props => props.img});*/`}
     border-radius: 15px;
     transition: all .5s;
-
-    ${props => !props.filePath && `
-      filter: grayscale(50%) !important;
-    `}
 
     & label {
       cursor: pointer;
@@ -133,36 +153,17 @@ const CardUpload = styled.div`
       justify-content: center;
       align-items: center;
       height: 100%;
-    }
-
-    &:hover {
-      transform: scale(1.005);
-      box-shadow: 0 8px 30px rgba(0,0,0,0.5);
-      
-    }
-
-    @media only screen and (min-width: 768px) and (max-width: 991px) {
-      width: 210px;
-      height: 168px;
-    }
-
-    @media(min-width: 992px) {
-      margin: 10px ${props => props.margin};
-    }
+    }    
   }
 
   & > label {
     margin: 20px auto;
     display: flex;
     justify-content: center;
-    width: 290px;
-    height: 230px;
     cursor: pointer;
     /* filter: blur(2px) !important; */
 
     align-items: center;
-    background-size: cover;
-    background-image: url(${props => props.img});
     border-radius: 15px;
     transition: all .5s;
 
@@ -177,11 +178,6 @@ const CardUpload = styled.div`
       color: #E57373;
     `}
 
-    &:hover {
-      transform: scale(1.005);
-      box-shadow: 0 8px 30px rgba(0,0,0,0.5);
-      
-    }
     @media only screen and (min-width: 768px) and (max-width: 991px) {
       width: 210px;
       height: 168px;
@@ -221,9 +217,11 @@ const DropActiveIcon = styled.div`
 `
 
 const CustomRow = styled.div`
-  min-height: 70vh;
+  // min-height: 70vh;
+  margin-top: 50px;
   display: flex;
   align-items: center;
+  // justify-content: center;
   padding-bottom: 40px;
 `
 
@@ -248,9 +246,10 @@ const Card = props => {
             <CardUpload
               {...props}
             >
-              <label
+              {/* <label
                 dangerouslySetInnerHTML={{ __html: `${content} ${showNumOfAsnwered(3)}` }}
-              />
+              /> */}
+              
             </CardUpload>
           </Link>
         ) : (
@@ -269,7 +268,7 @@ const Card = props => {
               <label
                 title={files[name].saving ? '' : props.title}
                 htmlFor={`${name}-file-input`}
-                dangerouslySetInnerHTML={{ __html: content }}
+                // dangerouslySetInnerHTML={{ __html: content }}
               />
               <AbsoluteContainer>
                 {
@@ -293,8 +292,40 @@ const Card = props => {
           </CardUpload>
         )
       }
+      {name=='parental_authorization'?<Download show/>:<Download/>}
     </div>
   )
+}
+
+const DownloadLink = styled.a`
+  color: #FFF;
+  text-decoration: underline;
+  font-size: 16px;
+  padding-top: 4px;
+  height: 24px;
+  display: block;
+  text-align: center;
+  width: auto;
+
+  &:link {
+    color: #FFF;
+  }
+
+  &:hover {
+    text-decoration: none;
+  }
+`
+
+const Download = props => {
+  return(
+    props.show?
+    <DownloadLink href='/static/file/parent_authorization.pdf'>
+      ดาวน์โหลดเอกสาร
+    </DownloadLink>
+    :
+    <DownloadLink/>
+  )
+  
 }
 
 const cardData = [
@@ -302,7 +333,7 @@ const cardData = [
     name: '1',
     outerClass: 'col-12 col-md-4 pr-md-0',
     margin: '0 0 auto',
-    img: '/static/img/upload-card-1.png',
+    img: '/static/img/card3.png',
     content: 'ตอบคำถาม <br/>',
     link: true,
     title: 'ไปหน้าตอบคำถาม'
@@ -311,7 +342,7 @@ const cardData = [
     name: 'transcription_record',
     outerClass: 'col-12 col-md-4 px-md-0',
     margin: 'auto 0',
-    img: '/static/img/upload-card-2.png',
+    img: '/static/img/card1.png',
     content: 'อัพโหลดใบ ปพ.1',
     isError: true,
     title: 'คลิก เพื่ออัพโหลดเอกสาร'
@@ -320,7 +351,7 @@ const cardData = [
     name: 'parental_authorization',
     outerClass: 'col-12 col-md-4 pl-md-0',
     margin: 'auto 0 0',
-    img: '/static/img/upload-card-1.png',
+    img: '/static/img/card2.png',
     content: 'อัพโหลดใบเอกสาร<br />ขออนุญาตผู้ปกครอง',
     isUpload: true,
     title: 'คลิก เพื่ออัพโหลดเอกสาร'
@@ -341,7 +372,6 @@ const MainUpload = props => {
                   key={index}
                   {...props}
                   {...data}
-
                 />
               ))
             }
--- utils/api.js
+++ utils/api.js
@@ -2,11 +2,13 @@ import axios from 'axios'
 
 const createInstance = () => {
   return axios.create({
-    baseURL: process.env.API,
+    // baseURL: process.env.API,
+    baseURL: 'http://localhost:8000/api/v1',
     headers: {
       // 'x-access-token': ''
       // 'Accept': 'application/json',
       // 'Content-Type': 'application/json'
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL3YxL2F1dGgvbG9naW4iLCJpYXQiOjE1MTgyNTY1MTAsImV4cCI6MTUxODI2MDExMCwibmJmIjoxNTE4MjU2NTEwLCJqdGkiOiJmZlFmS1Y4Y1R3eG82ZXU5Iiwic3ViIjoxMDAxMSwicHJ2IjoiODdlMGFmMWVmOWZkMTU4MTJmZGVjOTcxNTNhMTRlMGIwNDc1NDZhYSJ9.eoU5tR3iaGan8GgvHvs2GHqu8BU6NxjZxD3z9SCOpeA'
     }
   })
 }
