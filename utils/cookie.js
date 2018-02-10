94c94d95e3a1ce181c979ac2ea9e00d9048924b9
Keerati <mixxdarkz2@gmail.com>
2018-02-10 12:49:25 +0700

    [UPDATE] Update Logic in cookie Util

--- utils/cookie.js
+++ utils/cookie.js
@@ -1,15 +1,18 @@
+import {Router} from '../routes'
 import cookie from 'cookie'
 
 export default (ctx) => cookie.parse(
-  ctx.req ? ctx.req.headers.cookie : document.cookie
+  ctx.req && ctx.req.headers.cookie ? ctx.req.headers.cookie : document.cookie
 )
 
 export const checkToken = async (res, token, path) => {
-  if (token) {
+  if (res && token) {
     await res.writeHead(302, {
       Location: path
     })
-    res.end()
+    await res.end()
     res.finished = true
+  } else if (!token) {
+    Router.pushRoute(path)
   }
 }
