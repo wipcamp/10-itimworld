0b3daee0b0df1d5d3bf25d81dd46aafb5d11a5cb
JewlyTwin <l2jew555@hotmail.com>
2018-03-29 21:51:19 +0700

    [UPDATE] accrptcamper/main.js

--- components/AcceptCamper/Main.js
+++ components/AcceptCamper/Main.js
@@ -2,16 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 
 const BackgroundContainer = styled.div`
-    background-image: url("../../static/img/background.png");
-    min-height : 100vh;
-    width : 100%;
-    background-size : cover;
-    background-position : center;
+  background-image: url("../../static/img/background.png");
+  min-height : 100vh;
+  width : 100%;
+  background-size : cover;
+  background-position : center;
 `
 
 const Box = styled.div`
-    background-color : white;
-    
+  background-color : white;
+  padding : 2em;
 `
 
 const ModalContainer = styled.div`
@@ -27,9 +27,63 @@ const ModalContainer = styled.div`
   `}
 `
 
+const Where = styled.div`
+  min-height: 30px;
+`
+
+const Modal = (props) => (
+  <ModalContainer isShow={props.Show}>
+    <div className='container'>
+      <div className='row justify-content-center'>
+        <div className='col-10'>
+          <div className='mt-4 card'>
+            <div className='card-body'>
+              <h1 className='text-center'>ยืนยัน</h1>
+              <hr />
+              <div>
+                content
+              </div>
+              <hr />
+              <div className='row'>
+                <div className='col-6'>
+                  <button
+                    className='btn btn-block'
+                    onClick={props.toggle}
+                  >
+                    ยกเลิก
+                  </button>
+                </div>
+                <div className='col-6'>
+                  <button className='btn btn-success btn-block'>
+                    ยืนยัน
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </ModalContainer>
+)
+
 export default class index extends React.Component {
     state = {
-      isShow: false
+      isShow: false,
+      comeByYourself: ''
+    }
+
+    toggle = () => {
+      let isShow = this.state.isShow
+      this.setState({
+        isShow: !isShow
+      })
+    }
+
+    selectComeByYourself = (value) => {
+      this.setState({
+        comeByYourself: value
+      })
     }
 
     render () {
@@ -38,82 +92,69 @@ export default class index extends React.Component {
           <div className='container'>
             <div className='row justify-content-center'>
               <div className='col-10'>
-                <Box>
-                  <h2>ยืนยันสิทธิ</h2>
-                  <form>
-                    <input type='radio' name='comeByYourself' value='y' />มาเอง
-                    <input type='radio' name='comeByYourself' value='n' />มารับหน่อย
-                    <select name='size'>
-                      <option value=''>ไซส์เสื้อ</option>
-                      <option value='s'>s</option>
-                      <option value='m'>m</option>
-                      <option value='l'>l</option>
-                      <option value='xl'>xl</option>
-                    </select>
-                    <input type='file' />
-                  </form>
-                  <button>OK</button>
-                  <button>สละสิทธิ</button>
+                <Box className=''>
+                  <div>
+                    <h1>ยืนยันสิทธิ</h1>
+                    <form>
+                      <div className='col-6 form-check'>
+                        <input
+                          id='comeBy-y-input'
+                          type='radio'
+                          name='comeByYourself'
+                          value='y'
+                          onChange={() => this.selectComeByYourself('y')}
+                        />
+                        <label className='form-check-label lead' htmlFor='comeBy-y-input'>มาเอง</label>
+                      </div>
+                      <div className='col-8 form-check'>
+                        <input
+                          id='comeBy-y-input2'
+                          type='radio'
+                          name='comeByYourself'
+                          value='n'
+                          onChange={() => this.selectComeByYourself('n')}
+                        />
+                        <label className='form-check-label lead' htmlFor='comeBy-y-input2'>มารับหน่อย</label>
+                      </div>
+                      <Where>
+                        {
+                          this.state.comeByYourself === 'n' && (
+                            <select name='where'>
+                              <option value=''>ที่ไหนดี</option>
+                              <option value=''>1</option>
+                              <option value=''>2</option>
+                            </select>
+                          )
+                        }
+                      </Where>
+                      <div className='my-2'>
+                        <select name='size'>
+                          <option value=''>ไซส์เสื้อ</option>
+                          <option value='s'>s</option>
+                          <option value='m'>m</option>
+                          <option value='f'>f</option>
+                          <option value='l'>l</option>
+                          <option value='xl'>xl</option>
+                          <option value='2xl'>2xl</option>
+                          <option value='3xl'>3xl</option>
+                        </select> <br />
+                      </div>
+                      <input className='my-2' type='file' />
+                    </form>
+                    <div className='row'>
+                      <div className='col-6'>
+                        <button className='' onClick={this.toggle}>OK</button>
+                      </div>
+                      <div className='col-6'>
+                        <button className='' onClick={this.toggle}>สละสิทธิ</button>
+                      </div>
+                    </div>
+                  </div>
                 </Box>
               </div>
             </div>
           </div>
-          <ModalContainer isShow={this.state.isShow}>
-            <div className='container'>
-              <div className='row justify-content-center'>
-                <div className='col-10'>
-                  <div className='mt-4 card'>
-                    <div className='card-body'>
-                      <h1 className='text-center'>ยืนยัน</h1>
-                      <hr />
-                      <div>
-                        content
-                      </div>
-                      <hr />
-                      <div className='row'>
-                        <div className='col-6'>
-                          <button className='btn btn-block'>
-                            ยกเลิก
-                          </button>
-                        </div>
-                        <div className='col-6'>
-                          <button className='btn btn-success btn-block'>
-                            ยืนยัน
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </ModalContainer>
-
-          {/* <!-- Button trigger modal --> */}
-          <button type='button' className='btn btn-primary' data-toggle='modal' data-target='#exampleModal'>
-            Launch demo modal
-          </button>
-
-          {/* <!-- Modal --> */}
-          <div className='modal fade' id='exampleModal' tabIndex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-            <div className='modal-dialog' role='document'>
-              <div className='modal-content'>
-                <div className='modal-header'>
-                  <h5 className='modal-title' id='exampleModalLabel'>Modal title</h5>
-                  <button type='button' className='close' data-dismiss='modal' aria-label='Close'>
-                    <span aria-hidden='true'>&times;</span>
-                  </button>
-                </div>
-                <div className='modal-body'>
-                  ...
-                </div>
-                <div className='modal-footer'>
-                  <button type='button' className='btn btn-secondary' data-dismiss='modal'>Close</button>
-                  <button type='button' className='btn btn-primary'>Save changes</button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Modal Show={this.state.isShow} toggle={this.toggle} {...this.state} />
         </BackgroundContainer>
       )
     }
