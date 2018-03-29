9a1f3aaa52fafa016109ec83e6d8c832a355224b
DESKTOP-13DH8UD\papop <papop.ru@mail.kmutt.ac.th>
2018-03-29 20:37:12 +0700

    [UPDATE] confirm

--- pages/accept-camper/confirm.js
+++ pages/accept-camper/confirm.js
@@ -1,39 +1,18 @@
 import React from 'react'
-import styled from 'styled-components'
+import ConfirmFirst from '../../components/AcceptCamper/confirmFirst'
+import ConfirmTwo from '../../components/AcceptCamper/confirmTwo'
 
-const BackgroundContainer = styled.div`
-    background-image: url("../../static/img/background.png");
-    min-height : 100vh;
-    width : 100%;
-    background-size : cover;
-    background-position : center;
-    padding:30px;
-`
-const Box = styled.div`
-    background-color:white;
-    border-radius:5px;
-    padding:20px;
-`
-const DivButton = styled.div`
-    display:flex;
-    justify-content:space-around;
-`
 export default class index extends React.Component {
-    state = {}
+    state = {
+      change: false
+    }
 
     render () {
       return (
-        <BackgroundContainer>
-          <div className='container'>
-            <Box>
-              <div className='d-flex justify-content-center'>แน่ใจนะ</div>
-              <DivButton>
-                <button type='button' className='btn col-3'>back</button>
-                <button type='button' className='btn col-3'>ok</button>
-              </DivButton>
-            </Box>
-          </div>
-        </BackgroundContainer>
+        <div>
+          <ConfirmFirst />
+          <ConfirmTwo />
+        </div>
       )
     }
 }
