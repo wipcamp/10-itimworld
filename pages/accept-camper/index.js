bc0bde6a955404a9454a1b375864b8a8143e2ef1
SandzoNight <flukemdc@gmail.com>
2018-03-31 22:49:22 +0700

    [UPDATE] merge flow with announce-camper and protect route

--- pages/accept-camper/index.js
+++ pages/accept-camper/index.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import AcceptCamper from '../../components/AcceptCamper/Main'
+import serverRender from '../../utils/serverRender'
+import clientRender from '../../utils/clientRender'
+import { compose, lifecycle } from 'recompose'
+import campers from '../../components/custom/campers.json'
+import api from '../../utils/api'
+import cookie from '../../utils/cookie'
+import Router from 'next/router'
 
-export default class index extends React.Component {
-    state = {
-      isShow: false
-    }
+class index extends React.Component {
+  state = {
+    isShow: false
+  }
 
-    render () {
-      return (
-        <div>
-          <AcceptCamper />
-        </div>
-      )
-    }
+  async componentDidMount () {
+    
+  }
+  render () {
+    return (
+      <div>
+        <AcceptCamper />
+      </div>
+    )
+  }
 }
+
+export default compose(
+  clientRender(`/`),
+  serverRender(`/`),
+  lifecycle({
+    async componentWillMount () {
+      let {token} = await cookie({req: false})
+      let { data } = await api.post(`/auth/me`, null, {Authorization: `Bearer ${token}`})
+      let checkCamper = await campers.filter(el => {
+        return el.wipId === data.id
+      })
+      if (checkCamper.length === 0) {
+        Router.push('/announce/annoucement')
+      }
+    }})
+)(index)
