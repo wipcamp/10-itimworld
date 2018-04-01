c371ada07d0746d2fc5a2562ccc61a32249e979d
basinw <bas10600@gmail.com>
2018-04-01 14:32:55 +0700

    [UPDATE] Update custome/Announce add checkStep and fix flashlight

--- components/custom/Annouce.js
+++ components/custom/Annouce.js
@@ -1,51 +1,57 @@
 import React from 'react'
 import styled from 'styled-components'
 import Bg from '../../components/custom/Background'
+
+import { compose } from 'recompose'
+
+import checkStep from '../../utils/checkRegisterStep'
+import getToken from '../../utils/getToken'
+
 const Bg1 = styled.div`
   overflow-x:hidden;
   overflow-y:hidden;
 `
 
 const MouseFlashlight = styled.div`
-cursor: none;
-/* --cursorX: 50%;
---cursorY: 50%; */
-position: relative;
-display: inline-block;
-z-index:5;
-&:before {
-  content: '';
-  display: block;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  pointer-events: none;
-  background: radial-gradient(
-    circle 15vmax at ${props => props.cursorX + ' ' + props.cursorY},
-    rgba(0,0,0,0) 0%,
-    rgba(0,0,0,.5) 80%,
-    rgba(0,0,0,1) 100%
-  )
-}
+  cursor: none;
+  --cursorX: 50%;
+  --cursorY: 50%;
+  position: relative;
+  display: inline-block;
+  z-index:5;
+  &:before {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    pointer-events: none;
+    background: radial-gradient(
+      circle 15vmax at var(--cursorX) var(--cursorY),
+      rgba(0,0,0,0) 0%,
+      rgba(0,0,0,.5) 80%,
+      rgba(0,0,0,1) 100%
+    )
+  }
 `
 
-export default class App extends React.Component {
+class AnnounceWithFlashlight extends React.Component {
   state = {
-    x: '50%',
-    y: '60%',
     node: null
   }
 
   update = (e) => {
-    let x = e.clientX || e.touches[0].clientX
-    let y = e.clientY || e.touches[0].clientY
-    const { node } = this.state
-    x -= node.offsetLeft
-    y -= node.offsetTop
-    this.setState({
-      x: x + 'px',
-      y: y + 'px'
-    })
+    try {
+      let x = e.clientX || e.touches[0].clientX
+      let y = e.clientY || e.touches[0].clientY
+      const { node } = this.state
+      x -= node.offsetLeft
+      y -= node.offsetTop
+      node.style.setProperty('--cursorX', x + 'px')
+      node.style.setProperty('--cursorY', y + 'px')
+    } catch (err) {
+      /* ignore error */
+    }
   }
 
   loadRef = (node) => {
@@ -53,19 +59,21 @@ export default class App extends React.Component {
   }
 
   render () {
-    const { x, y } = this.state
     return (
       <Bg1>
         <MouseFlashlight
-          cursorX={x}
-          cursorY={y}
           onMouseMove={this.update}
           onTouchMove={this.update}
           innerRef={this.loadRef}
         >
-          <Bg />
+          <Bg  {...this.props}/>
         </MouseFlashlight>
       </Bg1>
     )
   }
 }
+
+export default compose(
+  getToken(),
+  checkStep('/announce/annoucement')
+)(AnnounceWithFlashlight)
