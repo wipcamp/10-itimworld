9c24e084f9995bc0948e20634ed53c2aaf6fb3a7
basinw <bas10600@gmail.com>
2018-04-01 14:34:32 +0700

    [UPDATE] annouce page add utils c/s render, withRedux

--- pages/announce/annoucement.js
+++ pages/announce/annoucement.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Annouce from '../../components/custom/Annouce'
+import { compose } from 'recompose'
 
-export default class Test extends React.Component {
-  render () {
-    return (
-      <div><Annouce /></div>
-    )
-  }
-}
+import withRedux from '../../store/wrapper'
+import serverRender from '../../utils/serverRender'
+import clientRender from '../../utils/clientRender'
+import Messenger from '../../components/Core/Messenger'
+
+const Announcement = (props) => (
+  <Annouce {...props} />
+)
+
+export default compose(
+  withRedux(),
+  clientRender(`/`),
+  serverRender(`/`),
+  Messenger
+)(Announcement)
--- pages/announce/index.js
+++ pages/announce/index.js
@@ -2,6 +2,14 @@ import React from 'react'
 import Custom from '../../components/custom/index'
 import Linktoannouce from '../../components/custom/linktoannouce'
 import styled from 'styled-components'
+import { compose } from 'recompose'
+
+import MainAnnounce from '../../components/custom/Main'
+
+import withRedux from '../../store/wrapper'
+import serverRender from '../../utils/serverRender'
+import clientRender from '../../utils/clientRender'
+import Messenger from '../../components/Core/Messenger'
 
 const Bg = styled.div`
   background-image: url('../static/img/bg1.png');
@@ -10,15 +18,14 @@ const Bg = styled.div`
   background-attachment: fixed;
   background-size: cover;
 `
-export default class Index extends React.Component {
-  render () {
-    return (
-      <div>
-        <Bg>
-          <Custom />
-          <Linktoannouce />
-        </Bg>
-      </div>
-    )
-  }
-}
+
+const Announce = (props) => (
+  <MainAnnounce {...props} />
+)
+
+export default compose(
+  withRedux(),
+  clientRender(`/`),
+  serverRender(`/`),
+  Messenger
+)(Announce)
