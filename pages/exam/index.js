7cf6ac49304eb452deea9b52444b9c0dfec6addf
SandzoNight <flukemdc@gmail.com>
2018-05-27 06:04:54 +0700

    [ADD] Add Exam step 3

--- pages/exam/index.js
+++ pages/exam/index.js
@@ -9,6 +9,7 @@ import clientRender from '../../utils/clientRender'
 import MainExam from '../../components/Exam/Main'
 import Step1 from '../../components/Exam/Step1'
 import Step2 from '../../components/Exam/Step2'
+import Step3 from '../../components/Exam/Step3'
 
 class ExamPage extends React.Component {
   render () {
@@ -18,6 +19,7 @@ class ExamPage extends React.Component {
         {error && <h1>ERROR</h1>}
         {error === false && step === 1 && <Step1 />}
         {error === false && step === 2 && <Step2 />}
+        {error === false && step === 3 && <Step3 />}
       </MainExam>
     )
   }
