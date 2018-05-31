8af227a7b45b762b61b0348af352b8bc7871ea64
SandzoNight <flukemdc@gmail.com>
2018-05-31 21:36:10 +0700

    [UPDATE] Implemented socket.io in Step1

--- components/Exam/Step1.js
+++ components/Exam/Step1.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
+import io from 'socket.io-client'
 
 import { compose } from 'recompose'
 import { connect } from 'react-redux'
@@ -15,6 +16,15 @@ class Step1 extends React.Component {
     super(props)
     this.start = this.start.bind(this)
   }
+  componentDidMount () {
+    this.socket = io()
+    this.socket.on('examStart', data => {
+      console.log(data)
+      if (data.status === 'start') {
+        this.start()
+      }
+    })
+  }
   start () {
     this.props.fetchExam()
   }
@@ -25,6 +35,10 @@ class Step1 extends React.Component {
         <div className='row'>
           <div className='col-12'>
             <h1>เจ้าพร้อมหรือยัง ?</h1>
+            <p>1. การสอบจักมีเวลาทั้งหมดยี่สิบนาที</p>
+            <p>2. ณ การทดสอบนี้ ออเจ้าจักมี ข้อกากบาท และ ข้อเขียน</p>
+            <p>3. อย่าคิดจะโกงแม้เพียงนิด แต่หากเป็นเช่นนั้นเจ้ากำลังดูหมิ่นสติปัญญาของตัวเจ้าเอง</p>
+            <p>4. ข้าอวยพรให้เจ้าได้เพียงโชคดี</p>
           </div>
         </div>
         <div className='row'>
