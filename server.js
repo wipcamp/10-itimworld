2720e69fcb31cf5a301d3552c52184e68b05e7c3
SandzoNight <flukemdc@gmail.com>
2018-06-01 19:50:38 +0700

    [UPDATE] Tried fixing socket.io-client

--- server.js
+++ server.js
@@ -7,25 +7,26 @@ const routes = require('./routes')
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
+const handle = app.getRequestHandler()
 
-const handle = routes.getRequestHandler(app, ({req, res, route, query}) => {
-  app.render(req, res, '/', query)
+// const handle = routes.getRequestHandler(app, ({req, res, route, query}) => {
+  // app.render(req, res, '/', query)
   // app.render(req, res, route.page, query)
-})
+// })
 
 const express = require('express')
 const server = express()
 const socketServer = require('http').Server(server)
 const io = require('socket.io')(socketServer)
 
-io.on('connection', socket => {
-  socket.on('examStart', data => {
-    io.emit('examStart', {status: 'start'})
-  })
-})
-
 app.prepare()
   .then(() => {
+    io.on('connection', socket => {
+      socket.on('examStart', data => {
+        io.emit('examStart', {status: 'start'})
+      })
+    })
+
     server.get('/robot.txt', (req, res) => {
       return res.sendFile(path.join(__dirname, './static', 'robot.txt'))
     })
@@ -33,7 +34,10 @@ app.prepare()
     server.get('/static/img/favicon/*', (req, res) => {
       return res.sendFile(path.join(__dirname, req.url))
     })
-    server.use(handle)
+    // server.use(handle)
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
     socketServer.listen(port, (err) => {
       if (err) throw err
       console.log(`> Ready on http://localhost:${port} eiei`)
