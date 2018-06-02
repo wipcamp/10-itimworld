e1fa3055db17b3627f92ef5de71c5eb85be2e420
SandzoNight <flukemdc@gmail.com>
2018-06-03 04:21:11 +0700

    [UPDATE] Change timer UI

--- components/Exam/Step2.js
+++ components/Exam/Step2.js
@@ -24,7 +24,7 @@ const Fixed = styled.div`
 const FixedContent = styled.div`
   border-radius: 5px 0 0 5px;
   background: orange;
-  font-size: 200%;
+  font-size: 170%;
   font-weight: bold;
 `
 
@@ -149,7 +149,9 @@ class Step2 extends React.Component {
       <div className='container-fluid'>
         <Fixed className='row'>
           <FixedContent className='col-12 p-4'>
-            เหลือเวลาอีก {this.state.minutes} นาที {this.state.seconds} วินาที
+            เหลือเวลาอีก
+            <br />
+            {this.state.minutes} นาที {this.state.seconds} วินาที
           </FixedContent>
         </Fixed>
         <div className='row d-none'>
