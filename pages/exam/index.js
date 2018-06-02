3de75266b73fcc51b6b7c2b57df0760bd4ebff5c
SandzoNight <flukemdc@gmail.com>
2018-06-03 04:17:28 +0700

    [UPDATE] Disabled key press function

--- pages/exam/index.js
+++ pages/exam/index.js
@@ -18,7 +18,18 @@ import cookie from '../../utils/cookie'
 import { actions as ExamActions } from '../../store/reducers/exam'
 
 class ExamPage extends React.Component {
-  componentDidMount () {
+  async componentDidMount () {
+    document.getElementsByTagName('html')[0].addEventListener('contextmenu', (ev) => {
+      ev.preventDefault()
+      return false
+    }, false)
+    document.getElementsByTagName('html')[0].addEventListener('keydown', (ev) => {
+      if (ev.keyCode !== 13 && ev.keyCode !== 8 && (ev.keyCode < 48 || ev.keyCode > 57) && (ev.keyCode > 105 || ev.keyCode < 96)) {
+        ev.preventDefault()
+        return false
+      }
+      return true
+    }, false)
     this.checkWipRole()
   }
   async checkWipRole () {
