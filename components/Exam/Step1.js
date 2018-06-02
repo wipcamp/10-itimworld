bb0b7af22cf501325d129627e990e15ae33401b3
basinw <bas10600@gmail.com>
2018-06-03 02:44:17 +0700

    [UPDATE] Styling Exam step1 like a card

--- components/Exam/Step1.js
+++ components/Exam/Step1.js
@@ -10,6 +10,11 @@ const Button = styled.button`
   width: 200px;
   height: 100px;
   font-size: 36px;
+  cursor: pointer;
+`
+
+const OrderList = styled.ol`
+  font-size: 130%;
 `
 class Step1 extends React.Component {
   constructor (props) {
@@ -30,26 +35,43 @@ class Step1 extends React.Component {
   render () {
     const {isAdmin} = this.props.exam
     return (
-      <div className='d-flex flex-column justify-content-center align-items-center'>
-        <div className='row'>
-          <div className='col-12'>
-            <h1>เจ้าพร้อมหรือยัง ?</h1>
-            <p>1. การสอบจักมีเวลาทั้งหมดยี่สิบนาที</p>
-            <p>2. ณ การทดสอบนี้ ออเจ้าจักมี ข้อกากบาท และ ข้อเขียน</p>
-            <p>3. อย่าคิดจะโกงแม้เพียงนิด แต่หากเป็นเช่นนั้นเจ้ากำลังดูหมิ่นสติปัญญาของตัวเจ้าเอง</p>
-            <p>4. ข้าอวยพรให้เจ้าได้เพียงโชคดี</p>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='col-12'>
-            {!isAdmin ? <p>กรุณารอสักประเดี๋ยว ...</p>
-              : <Button disabled={!isAdmin} onClick={this.start}>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-md-10 col-12 bg-white mt-5 rounded p-4'>
+            <div className=''>
+              <h1 className='text-center'>เจ้าพร้อมหรือยัง ?</h1>
+              <OrderList>
+                <li>การสอบจักมีเวลาทั้งหมดยี่สิบนาที</li>
+                <li>ณ การทดสอบนี้ ออเจ้าจักมี ข้อกากบาท และ ข้อเขียน</li>
+                <li>อย่าคิดจะโกงแม้เพียงนิด แต่หากเป็นเช่นนั้นเจ้ากำลังดูหมิ่นสติปัญญาของตัวเจ้าเอง</li>
+                <li>ข้าอวยพรให้เจ้าได้เพียงโชคดี</li>
+              </OrderList>
+            </div>
+            <div className='text-center'>
+              {!isAdmin ? <p>กรุณารอสักประเดี๋ยว ...</p>
+                : <Button className='btn btn-danger' disabled={!isAdmin} onClick={this.start}>
                 Start!
-              </Button>
-            }
+                </Button>
+              }
+            </div>
           </div>
         </div>
       </div>
+      // <div className='d-flex flex-column justify-content-center align-items-center'>
+      //   <div className='row'>
+      //     <div className='col-12'>
+      //     </div>
+      //   </div>
+      //   <div className='row'>
+      //     <div className='col-12'>
+      //       {!isAdmin ? <p>กรุณารอสักประเดี๋ยว ...</p>
+      //         : <Button disabled={!isAdmin} onClick={this.start}>
+      //           Start!
+      //         </Button>
+      //       }
+      //     </div>
+      //   </div>
+      // </div>
     )
   }
 }
